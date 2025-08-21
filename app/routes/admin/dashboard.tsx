83cc35cd4a239aa6cfc50408d22ee206bee6cba9
JASON-LI-LAPTOP\jsnli <jason.li@tklabs.com>
2025-08-21 17:25:49 -0400

    Add header on the dashboard layout

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { Header } from "../../../components";
 
 const Dashboard = () => {
+  const user = { name: "Jason" }
   return (
-    <div>Dashboard Page</div>
+    <main className="dashboard wrapper">
+      <Header 
+          title={`Welcome ${user?.name ?? 'Guest'}!`}
+          description="Track activity, trends and popular destinations in real time"
+      />
+
+      Dashboard Page Contents
+    </main>
   )
 }
 
