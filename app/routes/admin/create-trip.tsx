9c7150309d3bb4e77b8ba0eb39bbd00ed132026f
Jason Li <lijason@sheridancollege.ca>
2025-08-25 12:36:02 -0400

    Implement AI trip generation with gemini

--- app/routes/admin/create-trip.tsx
+++ app/routes/admin/create-trip.tsx
@@ -8,13 +8,14 @@ import { useState } from 'react';
 import { world_map } from '~/constants/world_map';
 import { ButtonComponent } from '@syncfusion/ej2-react-buttons';
 import { account } from '~/appwrite/client';
+import { useNavigate } from 'react-router';
 
 export const loader = async () => {
     const response = await fetch('https://restcountries.com/v3.1/all?fields=name,flag');
     const data = await response.json();
 
     return data.map((country: any) => ({
-        name: country.flag + country.name.common,
+        name: country.flag + ' ' + country.name.common,
         coordinate: country.latlng,
         value: country.name.common,
         openStreetMap: country.maps?.openStreetMap,
@@ -23,6 +24,7 @@ export const loader = async () => {
 
 const createTrip = ({ loaderData }: Route.ComponentProps) => {
     const countries = loaderData as Country[];
+    const navigate = useNavigate();
 
     const [formData, setFormData] = useState<TripFormData>({
         country: countries[0]?.name || '',
@@ -66,8 +68,28 @@ const createTrip = ({ loaderData }: Route.ComponentProps) => {
         }
 
         try {
-            console.log('user', user);
-            console.log('formData', formData);
+            const { country, duration: numberOfDays, travelStyle, interest: interests, budget, groupType } = formData;
+            const response = await fetch('/api/create-trip', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    country,
+                    numberOfDays,
+                    travelStyle,
+                    interests,
+                    budget,
+                    groupType,
+                    userId: user.$id
+                })
+            })
+
+            const result: CreateTripResponse = await response.json();
+
+            if (result?.id) {
+                navigate(`/trips/${result.id}`);
+            } else {
+                console.error('Failed to generate a trip');
+            }
         } catch (e) {
             console.error('Error generating trip', e);
         } finally {
