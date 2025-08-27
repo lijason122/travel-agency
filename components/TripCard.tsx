ae3566f762fe1700f5942be4c9782a7b1177a133
Jason Li <lijason@sheridancollege.ca>
2025-08-27 15:38:42 -0400

    Navigate to the trip detail page from the dashboard -> trip card

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -7,7 +7,7 @@ const TripCard = ({ id, name, location, imageUrl, tags, price }: TripCardProps) 
 
     return (
         <Link 
-            to={path.pathname === '/' || path.pathname.startsWith('/travel') ? `/travel/${id}` : `/trips/${id}`}
+            to={`/trips/${id}`}
             className="trip-card"
         >
             <img src={imageUrl} alt={name} />
